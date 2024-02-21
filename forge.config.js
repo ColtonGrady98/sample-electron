6243c66553974a5472851985fcb9c8b33137594d
Colton Grady <colton.grady@gmail.com>
2024-02-21 13:43:31 -0800

    commenting out process.env references until we have values

--- forge.config.js
+++ forge.config.js
@@ -11,8 +11,8 @@ module.exports = {
     {
       name: '@electron-forge/maker-squirrel',
       config: {
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD
+        // certificateFile: './cert.pfx',
+        // certificatePassword: process.env.CERTIFICATE_PASSWORD
       }
     },
     {
@@ -46,15 +46,15 @@ module.exports = {
     }),
   ],
   // mac signing info
-  packagerConfig: {
-    osxSign: {},
-    // ...
-    osxNotarize: {
-      tool: 'notarytool',
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_PASSWORD,
-      teamId: process.env.APPLE_TEAM_ID
-    }
-    // ...
-  }
+  // packagerConfig: {
+  //   osxSign: {},
+  //   // ...
+  //   osxNotarize: {
+  //     tool: 'notarytool',
+  //     appleId: process.env.APPLE_ID,
+  //     appleIdPassword: process.env.APPLE_PASSWORD,
+  //     teamId: process.env.APPLE_TEAM_ID
+  //   }
+  //   // ...
+  // }
 };
